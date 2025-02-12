c9ae85519544afed455a5c6b7a1f781c9d1d1fe6
sup0rt <georgal0527@gmail.com>
2025-02-12 13:18:14 +0300

    code change

--- fibon/Program.cs
+++ fibon/Program.cs
@@ -8,8 +8,34 @@ namespace fibon
 {
     internal class Program
     {
+        static int Fibonacci(int n)
+        {
+            int n1 = 0;
+            int n2 = 1;
+            int sum;
+
+            for (int i = 2; i < n; i++)
+            {
+                sum = n1 + n2;
+                n1 = n2;
+                n2 = sum;
+            }
+
+            return n == 0 ? n1 : n2;
+        }
         static void Main(string[] args)
         {
+            int result = Fibonacci(5);
+            Console.WriteLine(result);
+            result = Fibonacci(10);
+            Console.WriteLine(result);
+            result = Fibonacci(15);
+            Console.WriteLine(result);
+            result = Fibonacci(20);
+            Console.WriteLine(result);
+            result = Fibonacci(25);
+            Console.WriteLine(result);
+            Console.ReadKey();
         }
     }
 }
