6b560b9b07a5bf7e300ae67fecdbe70999fcc7e0
sup0rt <georgal0527@gmail.com>
2025-02-12 14:07:42 +0300

    added code letters

--- letters/letters/Program.cs
+++ letters/letters/Program.cs
@@ -8,8 +8,22 @@ namespace letters
 {
     internal class Program
     {
+        static void SendMessage(string name, int msg)
+        {
+            Console.WriteLine("Hello, " + name + "! Count to " + msg);
+        }
         static void Main(string[] args)
         {
+            char[] letters = { 'f', 'r', 'e', 'd', ' ', 's', 'm', 'i', 't', 'h' };
+            string name = "";
+            int[] a = new int[10];
+            for (int i = 0; i < letters.Length; i++)
+            {
+                name += letters[i];
+                a[i] = i + 1;
+                SendMessage(name, a[i]);
+            }
+            Console.ReadKey();
         }
     }
 }
